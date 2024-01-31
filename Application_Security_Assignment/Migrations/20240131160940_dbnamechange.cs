cb89da2ad8d4116d5e940602e5a54057c06b1986
Aloysius <zerolimits45@gmail.com>
2024-02-01 00:10:54 +0800

    dbname change

---
+++ Application_Security_Assignment/Migrations/20240131160940_dbnamechange.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application_Security_Assignment.Migrations
+{
+    public partial class dbnamechange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
